f08ce49b8653cb31da425428dddc88eb95cbb58a
Uros20170112 <57675991+Uros20170112@users.noreply.github.com>
2023-02-27 16:06:10 +0100

    Napravljene konstante

---
+++ Zajednicki/src/main/java/konstante/Operacije.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konstante;
+
+/**
+ *
+ * @author kompic
+ */
+public class Operacije {
+
+    public static final int ULOGUJ_KORISNIKA = 1;
+    public static final int IZLOGUJ_KORISNIKA = 2;
+    public static final int UCITAJ_CLANOVE = 3;
+    public static final int UCITAJ_MESTA = 4;
+    public static final int UCITAJ_PROMENE_PAKETA = 5;
+    public static final int UCITAJ_PAKETE = 6;
+    public static final int ZAPAMTI_CLANA = 7;
+    public static final int PRETRAZI_CLANOVE = 8;
+    public static final int IZMENI_CLANA = 9;
+    public static final int OBRISI_CLANA = 10;
+    public static final int ZAPAMTI_PAKET = 11;
+    public static final int IZMENI_PAKET = 12;
+    public static final int PRETRAZI_PAKETE = 13;
+    public static final int ZAPAMTI_PRETPLATU = 14;
+    public static final int UCITAJ_PRETPLATE = 15;
+    public static final int PRETRAZI_PRETPLATE = 16;
+    public static final int VRATI_TRENERE = 17;
+    public static final int ZAPAMTI_TRENERA = 18;
+    public static final int IZMENI_TRENERA = 19;
+    public static final int OBRISI_TRENERA = 20;
+}
