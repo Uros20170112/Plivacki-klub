0449a4a463f036421a60980f8938e95d175000ba
Uros20170112 <57675991+Uros20170112@users.noreply.github.com>
2023-03-03 22:34:47 +0100

    Napravljena dokumentacija u klasi Trener

--- Zajednicki/src/main/java/domen/Trener.java
+++ Zajednicki/src/main/java/domen/Trener.java
@@ -17,7 +17,8 @@ import java.util.logging.Logger;
  *
  * @author kompic
  */
-public class Trener extends AbstractObject{
+public class Trener extends AbstractObject {
+
     private String trenerId;
     private String ime;
     private String prezime;
@@ -33,73 +34,142 @@ public class Trener extends AbstractObject{
         this.tipTrenera = tipTrenera;
     }
 
+    /**
+     * Vraca id trenera
+     *
+     * @return String trenerId
+     */
     public String getTrenerId() {
         return trenerId;
     }
 
+    /**
+     * Postavlja vrednost za id trenera
+     *
+     * @param trenerId
+     */
     public void setTrenerId(String trenerId) {
         this.trenerId = trenerId;
     }
 
+    /**
+     * Vraca ime trenera
+     *
+     * @return String ime
+     */
     public String getIme() {
         return ime;
     }
 
+    /**
+     * Postavlja vrednost za ime trenera
+     *
+     * @param ime
+     */
     public void setIme(String ime) {
         this.ime = ime;
     }
 
+    /**
+     * Vraca prezime trenera
+     *
+     * @return String prezime
+     */
     public String getPrezime() {
         return prezime;
     }
 
+    /**
+     * Postavlja vrednost za prezime trenera
+     *
+     * @param prezime
+     */
     public void setPrezime(String prezime) {
         this.prezime = prezime;
     }
 
+    /**
+     * Vraca koji je tip trenera
+     *
+     * @return String tipTrenera
+     */
     public String getTipTrenera() {
         return tipTrenera;
     }
 
+    /**
+     * Postavlja vrednost za tip trenera
+     *
+     * @param tipTrenera
+     */
     public void setTipTrenera(String tipTrenera) {
         this.tipTrenera = tipTrenera;
     }
 
+    /**
+     *
+     * @return vraca naziv tabele u bazi za clana kako bi mogao da se napravi
+     * upit
+     */
     @Override
     public String vratiImeTabele() {
         return "trener";
     }
 
+    /**
+     *
+     * @return vraca parametre iz tabele
+     */
     @Override
     public String vratiParametre() {
         return String.format("'%s', '%s', '%s', '%s'", trenerId, ime, prezime, tipTrenera);
     }
 
+    /**
+     *
+     * @return vraca ime primarnog kljuca
+     */
     @Override
     public String vratiPK() {
         return "trenerid";
     }
 
+    /**
+     * Vraca vrednost primarnog kljuca
+     *
+     * @return String trenerId
+     */
     @Override
     public String vratiVrednostPK() {
         return trenerId;
     }
 
+    /**
+     *
+     * @return vraca slozen primarni kljuc
+     */
     @Override
     public String vratiSlozenPK() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Prima ResultSet(tabelu) i pretvara je u listu objekata Trener i vraca je
+     *
+     * @param rs
+     * @return lista objekata Trener
+     * @throws SQLException ako dodje do greske prilikom izvrsavanja upita
+     */
     @Override
     public List<AbstractObject> RSuTabelu(ResultSet rs) {
         List<AbstractObject> treneri = new ArrayList<>();
         try {
-            while(rs.next()) {
+            while (rs.next()) {
                 String trenerId = rs.getString("trenerId");
                 String ime = rs.getString("ime");
                 String prezime = rs.getString("prezime");
                 String tipTrenera = rs.getString("tipTrenera");
-                
+
                 Trener t = new Trener(trenerId, ime, prezime, tipTrenera);
                 treneri.add(t);
             }
@@ -109,16 +179,30 @@ public class Trener extends AbstractObject{
         return treneri;
     }
 
+    /**
+     *
+     * @return vraca formu za pravljenje SQL upita za update
+     */
     @Override
     public String vratiUpdate() {
         return String.format("trenerId = '%s', ime = '%s', prezime = '%s', tipTrenera = '%s'", trenerId, ime, prezime, tipTrenera);
     }
 
+    /**
+     * Postavlja vrednost primarnog kljuca
+     *
+     * @param pk
+     */
     @Override
     public void postaviVrednostPK(String pk) {
-        
+        this.trenerId = pk;
     }
 
+    /**
+     * Vraca String sa bitnim informacijama o treneru
+     *
+     * @return ime i prezime trenera
+     */
     @Override
     public String toString() {
         return ime + " " + prezime;
@@ -130,6 +214,16 @@ public class Trener extends AbstractObject{
         return hash;
     }
 
+    /**
+     * Poredi dva objekta Mesto i utvrdjuje da li su isti
+     *
+     * @param obj
+     * @return
+     * <ul>
+     * <li>true ako su oba objekta klase Trener i imaju isti mestoId
+     * <li>false u svakom drugom slucaju
+     * </ul>
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -147,6 +241,5 @@ public class Trener extends AbstractObject{
         }
         return true;
     }
-    
-    
+
 }
