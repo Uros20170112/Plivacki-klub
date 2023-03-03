2450d22770711d2ef1bd68411e3ed9fd942a985d
Uros20170112 <57675991+Uros20170112@users.noreply.github.com>
2023-03-03 22:04:22 +0100

    Napravljena dokumentacija u klasi Paket

--- Zajednicki/src/main/java/domen/Paket.java
+++ Zajednicki/src/main/java/domen/Paket.java
@@ -19,6 +19,9 @@ public class Paket extends AbstractObject {
     private String paketId;
     private String naziv;
     private String cena;
+    /**
+     * Lista termina koji pripadaju paketu
+     */
     private List<Termin> termini;
 
     public Paket(String paketId, String naziv, String cena) {
@@ -29,61 +32,106 @@ public class Paket extends AbstractObject {
 
     public Paket() {
     }
-
+    /**
+     * Vraca String sa bitnim informacijama o paketu
+     *
+     * @return naziv i cenu clana
+     */
     @Override
     public String toString() {
         return naziv + " - " + cena + " RSD";
     }
-
+/**
+ * Vraca id paketa
+ * @return String paketId
+ */
     public String getPaketId() {
         return paketId;
     }
-
+/**
+ * Postavlja vrednost id paketa
+ * @param paketId 
+ */
     public void setPaketId(String paketId) {
         this.paketId = paketId;
     }
-
+/**
+ * Vraca naziv paketa
+ * @return String naziv
+ */
     public String getNaziv() {
         return naziv;
     }
-
+/**
+ * Postavlja vrednost za naziv paketa
+ * @param naziv 
+ */
     public void setNaziv(String naziv) {
         this.naziv = naziv;
     }
-
+/**
+ * Vraca cenu paketa
+ * @return String cena
+ */
     public String getCena() {
         return cena;
     }
-
+/**
+ * Postavlja vrednost za cenu paketa
+ * @param cena 
+ */
     public void setCena(String cena) {
         this.cena = cena;
     }
-
+    /**
+     *
+     * @return vraca naziv tabele u bazi za clana kako bi mogao da se napravi
+     * upit
+     */
     @Override
     public String vratiImeTabele() {
         return "paket";
     }
-
+    /**
+     *
+     * @return vraca parametre iz tabele
+     */
     @Override
     public String vratiParametre() {
         return String.format("'%s', '%s', '%s'", paketId, naziv, cena);
     }
-
+    /**
+     *
+     * @return vraca ime primarnog kljuca
+     */
     @Override
     public String vratiPK() {
         return "paketId";
     }
-
+    /**
+     * Vraca vrednost primarnog kljuca
+     *
+     * @return String paketId
+     */
     @Override
     public String vratiVrednostPK() {
         return paketId;
     }
-
+    /**
+     *
+     * @return vraca slozen primarni kljuc
+     */
     @Override
     public String vratiSlozenPK() {
         return null;
     }
-
+    /**
+     * Prima ResultSet(tabelu) i pretvara je u listu objekata Paket i vraca je
+     *
+     * @param rs
+     * @return lista objekata paket
+     * @throws SQLException ako dodje do greske prilikom izvrsavanja upita
+     */
     @Override
     public List<AbstractObject> RSuTabelu(ResultSet rs) {
         List<AbstractObject> paketi = new ArrayList<>();
@@ -102,17 +150,33 @@ public class Paket extends AbstractObject {
         }
         return paketi;
     }
-
+    /**
+     *
+     * @return vraca formu za pravljenje SQL upita za update
+     */
     @Override
     public String vratiUpdate() {
         return String.format("paketId='%s',naziv='%s',cena='%s'", paketId, naziv, cena);
     }
-
+    /**
+     * Postavlja vrednost primarnog kljuca
+     *
+     * @param pk
+     */
     @Override
     public void postaviVrednostPK(String pk) {
         this.paketId = pk;
     }
-
+    /**
+     * Poredi dva objekta Paket i utvrdjuje da li su isti
+     *
+     * @param obj
+     * @return
+     * <ul>
+     * <li>true ako su oba objekta klase Paket i imaju isti paketId
+     * <li>false u svakom drugom slucaju
+     * </ul>
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -130,11 +194,17 @@ public class Paket extends AbstractObject {
         }
         return true;
     }
-
+/**
+ * Vraca listu termina koji se nalaze u paketu
+ * @return List<Termin> termini
+ */
     public List<Termin> getTermini() {
         return termini;
     }
-
+/**
+ * Postavlja vrednost za listu termini
+ * @param termini 
+ */
     public void setTermini(List<Termin> termini) {
         this.termini = termini;
     }
