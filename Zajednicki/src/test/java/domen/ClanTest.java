305228994465ead14a883d8c40c08676342d8b80
Uros20170112 <57675991+Uros20170112@users.noreply.github.com>
2023-03-09 17:27:16 +0100

    Doradjen ClanTest

--- Zajednicki/src/test/java/domen/ClanTest.java
+++ Zajednicki/src/test/java/domen/ClanTest.java
@@ -36,11 +36,11 @@ public class ClanTest {
 
     public ClanTest() {
     }
-    
-        @BeforeAll
+
+    @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
@@ -60,7 +60,7 @@ public class ClanTest {
     @Test
     public void testSetClanId() {
         c.setClanId("1");
-        
+
         assertEquals("1", c.getClanId());
     }
 
@@ -94,13 +94,13 @@ public class ClanTest {
         assertThrows(java.lang.NullPointerException.class,
                 () -> c.setIme(null));
     }
-    
+
     @Test
     public void testSetImeManjeOdDva() {
         assertThrows(java.lang.RuntimeException.class,
                 () -> c.setIme("P"));
     }
-    
+
     @Test
     public void testSetPrezime() {
         c.setIme("Peric");
@@ -113,11 +113,101 @@ public class ClanTest {
         assertThrows(java.lang.NullPointerException.class,
                 () -> c.setPrezime(null));
     }
-    
+
     @Test
     public void testSetPrezimeManjeOdDva() {
         assertThrows(java.lang.RuntimeException.class,
                 () -> c.setPrezime("P"));
+    }
+
+    @Test
+    public void testSetEmail() {
+        c.setEmail("pera@gmail.com");
+
+        assertEquals("pera@gmail.com", c.getEmail());
+    }
+
+    @Test
+    public void testSetEmailNull() {
+        assertThrows(java.lang.NullPointerException.class,
+                () -> c.setEmail(null));
+    }
+
+    public void testSetEmailNemaLudoA() {
+        assertThrows(java.lang.RuntimeException.class,
+                () -> c.setEmail("peragmail.com"));
+    }
+
+    @Test
+    public void testSetAdresa() {
+        c.setIme("Vojvode Stepe");
+
+        assertEquals("Vojvode Stepe", c.getPrezime());
+    }
+
+    @Test
+    public void testSetAdresaNull() {
+        assertThrows(java.lang.NullPointerException.class,
+                () -> c.setAdresa(null));
+    }
+
+    @Test
+    public void testSetAdresaManjeOdDva() {
+        assertThrows(java.lang.RuntimeException.class,
+                () -> c.setAdresa("V"));
+    }
+
+    /**
+     * Test of setTelefon method, of class Clan.
+     */
+    @Test
+    public void testSetTelefon() {
+        c.setTelefon("+38106412345678");
+        assertEquals("+38106412345678", c.getTelefon());
+    }
+
+    @Test
+    public void testSetTelefonNull() {
+        assertThrows(java.lang.NullPointerException.class, () -> c.setTelefon(null));
+    }
+
+    /**
+     * Test of setTelefon method, of class Clan.
+     */
+    @Test
+    public void testSetTelefonFormat() {
+        assertThrows(java.lang.RuntimeException.class, () -> c.setTelefon("abc123"));
+    }
+
+    /**
+     * Test of setTelefon method, of class Clan.
+     */
+    @Test
+    public void testSetTelefonDuzina() {
+        assertThrows(java.lang.RuntimeException.class, () -> c.setTelefon("0646412"));
+    }
+
+    /**
+     * Test of setMesto method, of class Clan.
+     */
+    @Test
+    public void testSetMesto() {
+        Mesto m1 = new Mesto();
+
+        m.setNaziv("Beograd");
+        m1.setNaziv("Beograd");
+
+        c.setMesto(m);
+
+        assertEquals(m1, c.getMesto());
+    }
+
+    /**
+     * Test of setMesto method, of class Clan.
+     */
+    @Test
+    public void testSetMestoNull() {
+        assertThrows(java.lang.NullPointerException.class, () -> c.setMesto(null));
     }
 
 }
